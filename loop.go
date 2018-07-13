0543f0efaf76b18cea0296dca08e913f73118a23
moving7 <xw_run@qq.com>
2018-07-14 04:11:56 +0800

    readfile

---
+++ loop.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"os"
+	"bufio"
+)
+
+func main() {
+	fmt.Println(
+		converToBin(5), //101
+		converToBin(10), //1101
+		converToBin(10219012),
+		converToBin(0),
+	)
+
+	readFile("start/abc.log")
+}
+
+func converToBin (n int) string {
+	result := ""
+	for ; n > 0; n /= 2 {
+		lsb := n % 2
+		result = strconv.Itoa(lsb) + result
+	}
+	return result
+}
+
+//读取文件
+func readFile (filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic (err)
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+}
