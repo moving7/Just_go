40147ad39558178e9a38fbdc2d6b0ee6b2b83c97
moving7 <xw_run@qq.com>
2018-07-14 01:46:58 +0800

    const

---
+++ const.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"math"
+	"fmt"
+)
+
+func main() {
+	consts()
+}
+
+func consts() {
+	//const filename string =  "abb.log"
+	//const a, b = 3, 4
+	const (
+		filename = "ab.log"
+		a, b = 3, 4
+		)
+	var c int
+	c = int(math.Sqrt(a*a + b*b))
+	fmt.Println(filename, c)
+}
