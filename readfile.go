d194a91ee550426892032fe4292db4068dd99647
moving7 <xw_run@qq.com>
2018-07-14 04:23:22 +0800

    readfile

---
+++ readfile.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"io/ioutil"
+	"fmt"
+)
+
+func main() {
+	const filename = "start/abc.log"
+	contents, err := ioutil.ReadFile(filename)
+	//if contents, err := ioutil.ReadFile(filename); err != nil {
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("%s \n", contents)
+	}
+}
