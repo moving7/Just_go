002ee28ee243415ad566c29603a32960113eff21
moving7 <xw_run@qq.com>
2018-07-15 03:10:00 +0800

    Longest Substring Without Repeating Characters

---
+++ second/find.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+	//lastOccurred[x]
+	/**
+	 * lastOccureed 查找指定内容最后出现的位置
+	 * 1. 从来没出现过 或者小于start 无需操作
+	 * 2. >= start 更新start
+	 */
+
+	 fmt.Println(
+	 	lengthOfNonRepeatingSubStr("abcabcdeaabcd"))
+	 fmt.Println(
+	 	lengthOfNonRepeatingSubStr("adskfladsflkwfiowe"))
+	 fmt.Println(
+	 	lengthOfNonRepeatingSubStr("测试测试请问考虑数量的咖啡机"))
+
+}
+
+func lengthOfNonRepeatingSubStr(s string) int {
+	lastOccerred := make(map[byte]int)
+	start := 0
+	maxLength := 0
+	for i, ch := range []byte(s) {
+		if lastI, ok := lastOccerred[ch]; ok && lastI >= start {
+			start = lastI + 1
+		}
+		if i - start + 1 > maxLength {
+			maxLength = i - start + 1
+		}
+		lastOccerred[ch] = i
+	}
+
+	return maxLength
+}
